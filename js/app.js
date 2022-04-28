07fc5461ddf0c014860c57707edbc3aa6408c473
Nick Bravo <nick@nickbravo.dev>
2022-04-27 22:39:26 -0600

    app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
-// Form validation and success message
+// Form validation
 (function() {
   'use strict';
   const forms = document.querySelectorAll('.needs-validation');
   Array.from(forms)
     .forEach(function(form) {
       form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
+        if (!form.checkValidity()) {
           event.preventDefault();
           event.stopPropagation();
         } else if (form.checkValidity() == true) {
           event.preventDefault();
-          $('#contact-form-success').modal('show');
+          form.submit();
         }
         form.classList.add('was-validated');
       }, false);
     });
 })();
+// Contact form success popup
+$(document).ready(function() {
+  if (window.location.href.indexOf('#success') != -1) {
+    $('#success').modal('show');
+  }
+});
